2ac7b099b7e97467af7f5c8e05428f8710bde4c4
Renato Bueno <renatobdo@gmail.com>
2023-09-07 09:44:25 -0300

    Arquivo de configuração
    Arquivo de configuração

---
+++ Semana5/config.php
@@ -0,0 +1,9 @@
+<?php
+// config.php
+return [
+    'TWILIO_SID' => 'Seu_SID_do_Twilio',
+    'TWILIO_AUTH_TOKEN' => 'Seu_Token_do_Twilio',
+    'TWILIO_TEL_NUMBER' => 'Seu_numero_tel_twilio'
+    // Outras configurações...
+];
+?>
